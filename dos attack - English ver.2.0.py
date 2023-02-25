a6b645bba5be00d847b0f4faf44b3c5fd25cc71f
System_HQmonkey <105772615+system-code-github@users.noreply.github.com>
2023-02-25 20:28:40 +0800

    Update dos attack - English ver.2.0.py

--- dos attack - English ver.2.0.py
+++ dos attack - English ver.2.0.py
@@ -59,13 +59,13 @@ class Main(Cmd):
 		exit(0)
 
 	def tutorial(self,chose): #Tutorial
-		print('1, you must use the "set_port" command to set the attack target port, typically 80' + \n)
+		print('1, you must use the "set_port" command to set the attack target port, typically 80\n')
 		sleep(1)
-		print('2, you must use the "set_ip" command to set the attack target IP address, note that it is the IP address!\nYou can get the IP address of the web address using the ping command + web address from the commond command line' + \n)
+		print('2, you must use the "set_ip" command to set the attack target IP address, note that it is the IP address!\nYou can get the IP address of the web address using the ping command + web address from the commond command line\n')
 		sleep(1)
-		print('3 you must use the "set_thread_num" command to set the number of attack threads.\nThe more threads, the more efficient the attack and the more likely the attack will succeed.\nBut not as much as possible. More computer cards are easy to use.\nThe recommended number is 500, depending on your computer.' + \n)
+		print('3 you must use the "set_thread_num" command to set the number of attack threads.\nThe more threads, the more efficient the attack and the more likely the attack will succeed.\nBut not as much as possible. More computer cards are easy to use.\nThe recommended number is 500, depending on your computer.\n')
 		sleep(1)
-		print('4, use the "attack" command, attack, type "y", you can start the attack, will show the number of attacks, and may be blacked by the site administrator at your own risk.' + \n)
+		print('4, use the "attack" command, attack, type "y", you can start the attack, will show the number of attacks, and may be blacked by the site administrator at your own risk.\n')
 	def set_thread_num(self,num):
 		self.thread = int(num)
 		print('Set thread ', num)
@@ -106,7 +106,7 @@ class Main(Cmd):
 	def true_to_attack(self): # Start Attack
 		self.true_to_attack_thread()
 # run attack
-print("use the 'tutorial' command to read the tutorial")
+print("use 'tutorial' command to read the tutorial")
 print()
 print()
 print()
